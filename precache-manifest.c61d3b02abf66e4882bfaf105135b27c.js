6bcbf878117b710681e96ae82159e84bf7ac5ed1
JasonLev <jasond.levy@gmail.com>
2019-06-27 14:44:18 -0700

    Updates

---
+++ precache-manifest.c61d3b02abf66e4882bfaf105135b27c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/drumkit-redux/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "15cf8fdac13a42de0991",
+    "url": "/drumkit-redux/static/css/main.11ff25d4.chunk.css"
+  },
+  {
+    "revision": "5a75e0751c1170902281",
+    "url": "/drumkit-redux/static/js/runtime~main.09a31a02.js"
+  },
+  {
+    "revision": "bc473b3a149a0bd01ab7",
+    "url": "/drumkit-redux/static/js/2.44006638.chunk.js"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drumkit-redux/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/drumkit-redux/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/drumkit-redux/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "15cf8fdac13a42de0991",
+    "url": "/drumkit-redux/static/js/main.05b0218e.chunk.js"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drumkit-redux/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drumkit-redux/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/drumkit-redux/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/drumkit-redux/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/drumkit-redux/static/media/tom.572ed39d.wav"
+  },
+  {
+    "revision": "b0fcbd9135481afc0d12478b6db900e4",
+    "url": "/drumkit-redux/index.html"
+  }
+];
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import nowPlaying from './nowPlaying'
-
-export default combineReducers({
-  nowPlaying
-})
